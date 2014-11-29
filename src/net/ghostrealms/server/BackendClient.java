8bf04fbb6d150b293e69e2c321073a28479e621e
River Marmorstein <river@rmarmorstein.com>
2014-11-29 15:01:44 -0500

    init files/boilerplate stuffz

---
+++ src/net/ghostrealms/server/BackendClient.java
@@ -0,0 +1,20 @@
+package net.ghostrealms.server;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by River on 29-Nov-14 14:46.
+ */
+public class BackendClient extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
