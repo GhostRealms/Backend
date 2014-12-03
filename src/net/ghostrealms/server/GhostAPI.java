44309b576eef2de301edcfcb601811067d0f0293
River Marmorstein <river@rmarmorstein.com>
2014-12-03 00:33:14 -0500

    added a method
    TODO create the return for the current logger object

--- src/net/ghostrealms/server/GhostAPI.java
+++ src/net/ghostrealms/server/GhostAPI.java
@@ -1,9 +1,15 @@
 package net.ghostrealms.server;
 
+import net.ghostrealms.server.log.RealmsLogger;
+
 /**
  * Created by River on 30-Nov-14.
  * Provides getters for common API components for the backend systems
  */
 public class GhostAPI {
+
+    public static RealmsLogger getLogger() {
+        return null;
+    }
 
 }
